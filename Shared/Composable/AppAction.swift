929a75fdfa9d8173df95c816985bb62336d52845
LeeCheolGu <cjfrn0443@gmail.com>
2021-11-19 18:33:35 +0900

    TCA 공부 - 파일 생성 및 적용 중

---
+++ Shared/Composable/AppAction.swift
@@ -0,0 +1,13 @@
+//
+//  AppAction.swift
+//  SwiftUIToturoial (iOS)
+//
+//  Created by 이철구 on 2021/11/19.
+//
+
+import Foundation
+
+enum CouterAction: Equatable {
+    case derementButton
+    case incrementButton
+}
---
+++ Shared/Composable/AppReducer.swift
@@ -0,0 +1,12 @@
+//
+//  AppReducer.swift
+//  SwiftUIToturoial (iOS)
+//
+//  Created by 이철구 on 2021/11/19.
+//
+
+import Foundation
+import ComposableArchitecture
+
+struct CouterEnvironment {}
+
